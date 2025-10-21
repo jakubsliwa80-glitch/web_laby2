56ca5e292f9663a0b7fa80f8b989ac76ba1ff07b
jakubsliwa80-glitch <jakubsliwa80@gmail.com>
2025-10-21 07:31:09 +0000

    2_1.4

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
   const cw1 = document.getElementById('cw1')
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
+  const cw4 = document.getElementById('cw4')
   const answer = document.getElementById('answer')
 
   example.addEventListener("click", function() {
@@ -67,6 +68,37 @@
         const postDiv = document.createElement('div');
         postDiv.innerHTML = `
         <h2>Post #${post.id} Użytkownika ${post.userId} - ${post.title}</h2>
+        <p>${post.body}</p>
+      `;
+        answer.appendChild(postDiv);
+
+      })
+  })
+
+  cw4.addEventListener("click", function() {
+
+    answer.innerHTML = 'Processing...';
+    setTimeout(1000);
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'przykladowy tytul',
+        body: 'przykladowe body',
+        userId: 4444,
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        }
+      })
+    })
+      .then(response => response.json())
+      .then(post => {
+
+        answer.innerHTML = '';
+
+        const postDiv = document.createElement('div');
+        postDiv.innerHTML = `
+        <h2>Dodano nowy post o id #${post.id} - ${post.title}</h2>
         <p>${post.body}</p>
       `;
         answer.appendChild(postDiv);
