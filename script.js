0b09c4cff3aa0ebc77d4913c4f5a88fdc15eab8d
jakubsliwa80-glitch <jakubsliwa80@gmail.com>
2025-10-21 07:10:00 +0000

    Com2_1.1

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-(function () {
+(function() {
   const example = document.getElementById('example')
   const cw1 = document.getElementById('cw1')
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
   const answer = document.getElementById('answer')
 
-  example.addEventListener("click", function () {
+  example.addEventListener("click", function() {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
@@ -14,15 +14,30 @@
       })
   })
 
-  cw1.addEventListener("click", function () {
+  cw1.addEventListener("click", function() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+
+        array.forEach(post => {
+          const postDiv = document.createElement('div');
+          postDiv.innerHTML = `
+          <h2>Post #${post.id} Użytkownika ${post.userId} - ${post.title}</h2>
+          <p>${post.body}</p>
+        `;
+          answer.appendChild(postDiv);
+
+
+        });
+
+      })
+  })
+
+  cw2.addEventListener("click", function() {
     //TODO
   })
 
-  cw2.addEventListener("click", function () {
-    //TODO
-  })
-
-  cw3.addEventListener("click", function () {
+  cw3.addEventListener("click", function() {
     //TODO
   })
 
